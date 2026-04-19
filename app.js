b99b084612d65ed22b454e382f726680e3df9762
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-04-19 23:50:06 +0530

    modified main node file

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const listing = require("./models/listings.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js"); 
+const session = require("express-session");
+const flash = require("connect-flash");
 
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -30,75 +31,37 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
+const sessionOptions = {
+    secret : "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        expires: Date.now()+7*24*60*60*1000,
+        maxAge: 7*24*60*60*1000,
+        httpOnly: true,
+    },
+};
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next)=>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
 
 app.get("/", (req, res)=>{
     res.send("Hello airbnb");
 });
 
-const validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-};
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 
-app.get("/listings",  wrapAsync(async(req, res)=>{
-    const allListings = await listing.find({});
-    res.render("listings/index.ejs", {allListings})
-}));
 
-//new route
-app.get("/listings/new", (req, res)=>{
-    res.render("listings/new.ejs");
-});
-
-//show route
-app.get("/listings/:id",  wrapAsync(async(req, res)=>{
-
-    let {id} = req.params;
-    const list = await listing.findById(id);
-    res.render("listings/show.ejs", {list});
-}));
-
-//create route
-app.post("/listings", validateListing,  wrapAsync(async(req, res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400, "Send valid data for listing");
-    }
-    const newList = new listing(req.body.listing);
-    await newList.save();
-    res.redirect("/listings");
-}));
-//edit route
-app.get("/listings/:id/edit",  wrapAsync(async(req, res)=>{
-    let {id} = req.params;
-    const Elisting = await listing.findById(id);
-    res.render("listings/edit.ejs", {Elisting});
-}));
-
-//update route
-app.put("/listings/:id", validateListing,  wrapAsync(async(req, res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400, "Send valid data for listing");
-    }
-    let {id} = req.params;
-    await listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`)
-}));
-
-//delete route
-app.delete("/listings/:id", wrapAsync(async(req, res)=>{
-    let {id} =req.params;
-    let deletedListing = await listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-app.all("*", (req, res, next)=>{
+app.all('{/*path}', (req, res, next)=>{
     next(new ExpressError(404, "Page Not Found!"));
 });
 
