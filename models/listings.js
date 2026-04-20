7710c32dc6aedb5f3c4163a6337b66022a277317
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-04-20 22:55:15 +0530

    Modified model

--- models/listings.js
+++ models/listings.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./reviews.js");
 
 const listingSchema = new Schema({
     title: {
@@ -11,16 +12,28 @@ const listingSchema = new Schema({
         filename: String,
         url:{
             type: String,
-            default: "https://unsplash.com/photos/high-rise-buildings-city-scape-photography-wpU4veNGnHg",
+            default: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             set: (v) =>
             v===""
-                ?"https://unsplash.com/photos/high-rise-buildings-city-scape-photography-wpU4veNGnHg"
+                ?"https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 :v,
         }
     },
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+});
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
 });
 
 const listing =mongoose.model("Listing", listingSchema);
