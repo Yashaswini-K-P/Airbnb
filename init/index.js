63705a2bc90e53a09cfe9f219a76df3849b68b97
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-04-22 23:12:18 +0530

    modified data

--- init/index.js
+++ init/index.js
@@ -16,6 +16,10 @@ async function main(){
 
 const initDB = async()=>{
     await listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+        ...obj,
+        owner: "69e7746753b2e1471b9c46a3"
+    }));
     await listing.insertMany(initData.data);
     console.log("data was initialized");
 };
