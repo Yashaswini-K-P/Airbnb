9b6b61edb9588693c1245fa9284e0197d9357660
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-05-01 00:07:55 +0530

    modified

--- schema.js
+++ schema.js
@@ -1,13 +1,16 @@
 const Joi =require('joi');
 
-module.exports.listingschema = Joi.object({
+module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null),
+        image: Joi.object({
+            url: Joi.string().allow("", null),
+            filename: Joi.string().allow("", null)
+        }).allow("", null)
     }).required(),
 });
 
