5a504053b454f7845a3b07cd7e7ba4675102fb89
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-07-01 10:15:56 +0530

    models file

--- models/listings.js
+++ models/listings.js
@@ -10,14 +10,7 @@ const listingSchema = new Schema({
     description: String,
     image: {
         filename: String,
-        url:{
-            type: String,
-            default: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            set: (v) =>
-            v===""
-                ?"https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                :v,
-        }
+        url: String
     },
     price: Number,
     location: String,
