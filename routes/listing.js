119f68858120a90d2032a1a62e9742223f45ed3c
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-07-07 22:36:55 +0530

    modified listing file

--- routes/listing.js
+++ routes/listing.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const listing = require("../models/listings.js");
+const Listing = require("../models/listings.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
-const { cloudinary } = require("../cloudConfig.js");
 
-const multer  = require('multer')
-const {storage} = require("../cloudConfig.js");
-const upload = multer({storage});
 
 const listingController = require("../controllers/listing.js");
 
 
 router.route("/")
     .get(wrapAsync(listingController.index))
-    // .post(
-    //     isLoggedIn,
-    //     validateListing,  
-    //     wrapAsync(listingController.createListing)
-    // );
-   .post(upload.single("listing[image]"), (req, res)=>{
-        res.send(req.file);
-   });
+    .post(
+        isLoggedIn,
+         validateListing,
+        wrapAsync(listingController.createListing)
+    );
+   
 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
@@ -35,7 +29,7 @@ router.route("/:id")
         validateListing,  
         wrapAsync(listingController.updateListing)
     )
-    .delete(isLoggedIn,  
+    .delete(isLoggedIn,  isOwner,
         wrapAsync(listingController.destroylisting)
     );
 
